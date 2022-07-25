8a651da6eabacc46340052a1a2feb23462a83382
Abdul-Basith-R <abubas002@gmail,com>
2022-07-25 18:45:58 +0530

    Third commit

--- app.py
+++ app.py
@@ -1,17 +1,26 @@
-from crypt import methods
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from sentence_transformers import SentenceTransformer, util
 
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+
 app = Flask(__name__)
-@app.route('/')
+@app.route('/', methods =["GET", "POST"])
 def home():
+    if request.method == "POST":
+           # getting input with name = fname in HTML form
+       sentence1 = request.form.get("s1")
+       # getting input with name = lname in HTML form
+       sentence2 = request.form.get("s2")
+       embedding_1 = model.encode(sentence1, convert_to_tensor=True)
+       embedding_2 = model.encode(sentence2, convert_to_tensor=True)
+       cosine_similarity = util.pytorch_cos_sim(embedding_1, embedding_2)
+       return str(cosine_similarity.item())
+
     return render_template("index.html")
 
-@app.route('/request/<sentences>',methods=["POST"])
+@app.route('/request/<sentences>',methods=["GET","POST"])
 def process_user(sentences):
     sentences = list(sentences.split(","))
-
-    model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
 #Compute embedding for both lists
     embedding_1 = model.encode(sentences[0], convert_to_tensor=True)
     embedding_2 = model.encode(sentences[1], convert_to_tensor=True)
